eec4ad831e84dcf84cce7b6747ff9a066557e5b0
Syed Maaz Ali Shah <saayyadsaifa003@gmail.com>
2026-04-05 21:29:07 -0700

    something new

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
 import './Navbar.css'
-
+import { NavLink, useNavigate } from 'react-router-dom'
 const Navbar = () => {
-
-    const handleLogIn = (e) =>{
-        document.open("/login")
+    const navigate = useNavigate()
+    const handleLogIn = (e) => {
+        navigate("/login")
     }
-    const handleSignUp = (e) =>{
-        document.open("/signup")
+    const handleSignUp = (e) => {
+        navigate("/signup")
     }
-
-
     return (
         <nav className="nav">
             <div className="navpart">
-                <span className="brandname">LinkShrink</span>
+                <span className="brandname">Shrink.It</span>
             </div>
             <div className="navpart">
                 <ul className="navlinks">
-                    <a href="/"><li className="navlink">Home</li></a>
-                    <a href="/shrink"><li className="navlink">Shrink</li></a>
-                    <a href="/contact"><li className="navlink">Contact</li></a>
-                    <a href="/about"><li className="navlink">About</li></a>
-                    {/* <a href=""><li className="navlink">Home</li></a>
-                <a href=""><li className="navlink">Home</li></a> */}
+                    <NavLink to="/"><li className="navlink">Home</li></NavLink>
+                    <NavLink to="/shrink"><li className="navlink">Shrink</li></NavLink>
+                    <NavLink to="/contact"><li className="navlink">Contact</li></NavLink>
+                    <NavLink to="/about"><li className="navlink">About</li></NavLink>
                 </ul>
             </div>
             <div className="navpart">
@@ -33,5 +29,4 @@ const Navbar = () => {
         </nav>
     )
 }
-
 export default Navbar
